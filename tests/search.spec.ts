2ec1fdde8497a67c0efe2fca6d5737273281cfb9
jfbridges <juanfbridges@hotmail.com>
2024-06-25 21:42:37 -0500

    resolucion del reto 2 jfpg

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,19 +1,19 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('/');
+  await page.goto('https://playwright.dev/');
 });
 
 test('Realizar una busqueda que no tenga resultados', async ({ page }) => {
-  await page.getByRole('button').click();
+  await page.getByRole('button',{name:'Search'}).click();
 
   await page.getByPlaceholder('Search docs').click();
 
   await page.getByPlaceholder('Search docs').fill('hascontent');
 
-  expect(page.locator('.DocSearch-NoResults p')).toBeVisible();
+  await page.locator('.DocSearch-NoResults p').isVisible();
 
-  expect(page.locator('.DocSearch-NoResults p')).toHaveText('No results for hascontent');
+  await expect(page.locator('.DocSearch-NoResults p')).toContainText('No results for "hascontent"');
 
 })
 
@@ -26,7 +26,7 @@ test('Limpiar el input de busqueda', async ({ page }) => {
 
   await searchBox.fill('somerandomtext');
 
-  await expect(searchBox).toHaveText('somerandomtext');
+  await expect(searchBox).toHaveAttribute('value','somerandomtext');
 
   await page.getByRole('button', { name: 'Clear the query' }).click();
 
@@ -34,7 +34,7 @@ test('Limpiar el input de busqueda', async ({ page }) => {
 });
 
 test('Realizar una busqueda que genere al menos tenga un resultado', async ({ page }) => {
-  await page.getByRole('button', { name: 'Search ' }).click();
+  await page.getByRole('button', { name: 'Search' }).click();
 
   const searchBox = page.getByPlaceholder('Search docs');
 
@@ -42,11 +42,11 @@ test('Realizar una busqueda que genere al menos tenga un resultado', async ({ pa
 
   await page.getByPlaceholder('Search docs').fill('havetext');
 
-  expect(searchBox).toHaveText('havetext');
+  await expect(searchBox).toHaveAttribute('value','havetext');
 
   // Verity there are sections in the results
   await page.locator('.DocSearch-Dropdown-Container section').nth(1).waitFor();
   const numberOfResults = await page.locator('.DocSearch-Dropdown-Container section').count();
-  await expect(numberOfResults).toBeGreaterThan(0);
+  expect(numberOfResults).toBeGreaterThan(0);
 
 });
